227368eae79acda65f2eff62197890e13b9fdac6
himanshuatwork21 <work.himanshubarman@gmail.com>
2026-05-07 09:34:14 +0530

    push

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -6,10 +6,11 @@ export const client = createClient({
   apiVersion: '2024-01-01',
   useCdn: false,
 });
+
 export const writeClient = createClient({
   projectId: 'xy9egrg6',
   dataset: 'production',
   apiVersion: '2024-01-01',
-  token: 'skQMOdr0KUcLstqCjIwfFEnx13uEXuJ2UPWR93Z8ElUhDkwRqqZxheXhbJEbsTeyzDfbQ3KJGqKoxmxsZqvjXZjCUhZJWWcmZ4Wuw7s6tIkEfn5OYTnWV8FNbEyv2jDPNlAd2kqKIl2b7ZVRcI2Aik5uDoHIQarZiqMzbtnKXzyw39G8cRGR',
+  token: import.meta.env.VITE_SANITY_TOKEN,
   useCdn: false,
 });
